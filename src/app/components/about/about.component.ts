77372ae8865c0d7681922a6128096f31e1a7e810
Cyril Attie <attiecyril@gmail.com>
2019-09-23 10:35:24 +0200

    Cryypto donations

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-
+import { faTwitter, faLinkedinIn} from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
-
+ 
+  faTwitter = faTwitter;
+  faLinkedinIn = faLinkedinIn;
   constructor() { }
 
 
